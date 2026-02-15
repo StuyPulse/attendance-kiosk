54f6701d943635a1f0e0449acdf5e393b430d9c3
Kevin Wang <kevin@kevinwang.com>
2026-02-14 23:30:35 -0500

    Gzip database backup before uploading to S3

--- src/backup.ts
+++ src/backup.ts
@@ -1,4 +1,6 @@
 import fs from "fs";
+import { buffer } from "stream/consumers";
+import { createGzip } from "zlib";
 import { Database } from "sqlite";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getTimestampedFilename } from "./util";
@@ -19,17 +21,19 @@ export async function backupDBToS3(db: Database) {
     }
 
     await db.run(`VACUUM INTO "${tmpBackupPath}"`);
-    const dbContents = await fs.promises.readFile(tmpBackupPath);
+    const dbStream = fs.createReadStream(tmpBackupPath);
+    const gzip = createGzip();
+    const gzippedDB = await buffer(dbStream.pipe(gzip));
 
     const client = new S3Client({ region: process.env.AWS_REGION });
     const bucketName = process.env.BACKUP_S3_BUCKET;
     const prefix = process.env.BACKUP_S3_PREFIX || "";
-    const filename = getTimestampedFilename("data", "db");
+    const filename = getTimestampedFilename("data", "db.gz");
 
     const command = new PutObjectCommand({
         Bucket: bucketName,
         Key: `${prefix}${filename}`,
-        Body: dbContents,
+        Body: gzippedDB,
     });
 
     await client.send(command);
